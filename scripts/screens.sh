305ead90bdbc2c0184190ddadff0df425068b3a3
T.G.Wahooka <wahooka@safe-mail.net>
2016-04-01 21:11:07 -0300

    start the pytrader processes over the screen in detached mode.

--- scripts/screens.sh
+++ scripts/screens.sh
@@ -1,4 +1,28 @@
-screen -S predictNN bash -c 'while true; do cd pytrader; ./manage.py predict_many_v2; done'
-screen -S predictSK bash -c 'while true; do cd pytrader; ./manage.py predict_many_sk; done'
-screen -S trade bash -c 'while true; do cd pytrader; ./manage.py trade; done'
-screen -S runserver bash -c 'while true; do cd pytrader; ./manage.py runserver 45.55.42.224:80; done'
+#!/bin/bash
+
+####################
+# Script to start some processes of pytrader like a service in screen terminals
+####################
+
+####################
+# Configuration
+
+# location of pytrader directory
+
+PYTRADERHOME="~/pytrader"
+
+# IP address and port to bind the django server
+# PYTRADERBIND="" # Use default value 127.0.0.1:8000
+# PYTRADERBIND="45.34.34.12:80" # Use a specific IP address and port
+
+PYTRADERBIND=""
+
+# Configuration
+####################
+
+screen -d -m -S predictNN bash -c 'while true; do cd ${PYTRADERHOME} ; ./manage.py predict_many_v2; done'
+screen -d -m -S predictSK bash -c 'while true; do cd ${PYTRADERHOME} ; ./manage.py predict_many_sk; done'
+screen -d -m -S trade bash -c 'while true; do cd ${PYTRADERHOME} ; ./manage.py trade; done'
+screen -d -m -S runserver bash -c 'while true; do cd ${PYTRADERHOME} ; ./manage.py runserver ${PYTRADERBIND} ; done'
+
+screen -list
